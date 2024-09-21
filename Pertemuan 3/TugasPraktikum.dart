13c8cb97d2797d982ffb6b2ed13f9e148f9a2103
ichaputri <90027141+ichaputri@users.noreply.github.com>
2024-09-21 22:35:26 +0700

    Fixed Tugas Praktikum 3

--- Pertemuan 3/TugasPraktikum.dart
+++ Pertemuan 3/TugasPraktikum.dart
@@ -2,16 +2,21 @@ void main() {
   String nama = "Icha Dewi Putriana";
   String nim = "2241720069";
 
-  for (int i = 1;; i++) {
+  for (int i = 0;; i++) {
     int angka = 2 * i - 1;
+    if (i == 0) angka = 2; // Kondisi khusus untuk angka 2
     if (angka > 201) break;
 
     // Mengecek apakah angka adalah bilangan prima
-    bool prima = angka > 0;
-    for (int j = 2; j * j <= angka; j++) {
-      if (angka % j == 0) {
-        prima = false;
-        break;
+    bool prima = true;
+    if (angka <= 1) {
+      prima = false;
+    } else {
+      for (int j = 2; j * j <= angka; j++) {
+        if (angka % j == 0) {
+          prima = false;
+          break;
+        }
       }
     }
 
