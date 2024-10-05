a7d427c8c1dd06952eb17c77d84aaf19ceea02f9
ichaputri <ichaputriana39@gmail.com>
2024-10-05 16:41:12 +0700

    Pertemuan 6 - Tugas 2

--- Pertemuan 6/belanja/lib/pages/home_page.dart
+++ Pertemuan 6/belanja/lib/pages/home_page.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import '../models/item.dart';
 
 class HomePage extends StatelessWidget {
   final List<Item> items = [
     Item(
-        name: 'Sugar', price: 5000),
-    Item(name: 'Telur', price: 26000)
+        name: 'Delmonte Saus Spaghetti 2 x 250 gr',
+        price: 10000,
+        imageUrl:
+            'https://down-id.img.susercontent.com/file/18c3d4f739ef28ee0d3249dcab2d8dc1.webp',
+        stock: 10,
+        rating: 4.9),
+    Item(
+        name: 'Heinz Apple Vinegar Cuka 473 mL',
+        price: 59000,
+        imageUrl:
+            'https://down-id.img.susercontent.com/file/70d49117dbb8fb105e1c3a88edf876b2.webp',
+        stock: 20,
+        rating: 4.8),
+    Item(
+        name: 'Saus Delmonte Extra Hot Chilli',
+        price: 22000,
+        imageUrl:
+            'https://down-id.img.susercontent.com/file/65fb5e15e7b2f6436363abb166913067.webp',
+        stock: 20,
+        rating: 4.8),
+    Item(
+        name: 'Kikkoman Kecap Asin 150 mL',
+        price: 22000,
+        imageUrl:
+            'https://down-id.img.susercontent.com/file/22396b67cf67ac691e8d8a990070d93e@resize_w450_nl.webp',
+        stock: 20,
+        rating: 4.8),
+    Item(
+        name: 'Pondan Tepung Brownies',
+        price: 16000,
+        imageUrl:
+            'https://down-id.img.susercontent.com/file/54a6e54da7444188f6d04d558a60e266.webp',
+        stock: 20,
+        rating: 4.8),
+    Item(
+        name: 'Finna Uleg Sambel Terasi 190gr',
+        price: 17000,
+        imageUrl:
+            'https://down-id.img.susercontent.com/file/a96a595c68f361425f3492afca226611.webp',
+        stock: 20,
+        rating: 4.8),
   ];
 
   @override
@@ -14,19 +52,24 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Shopping List'),
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.lightGreen,
       ),
       body: Container(
         margin: EdgeInsets.all(8),
         padding: EdgeInsets.all(8),
-        child: ListView.builder(
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, // Jumlah kolom
+            crossAxisSpacing: 10, // Spasi antar kolom
+            mainAxisSpacing: 10, // Spasi antar baris
+            childAspectRatio: 3 / 6, // Rasio lebar dan tinggi item
+          ),
           itemCount: items.length,
           itemBuilder: (context, index) {
             final item = items[index];
-            // Prak 5 - Langkah 7
             return InkWell(
               onTap: () {
-                // Navigasi ke ItemPage dengan item yang diklik
+                // Navigasi ke halaman item yang dipilih
                 Navigator.pushNamed(
                   context,
                   '/item',
@@ -34,23 +77,76 @@ class HomePage extends StatelessWidget {
                 );
               },
               child: Card(
-                child: Container(
-                  margin: EdgeInsets.all(8),
-                  child: Row(
-                    children: [
-                      Expanded(child: Text(item.name)),
-                      Expanded(
-                        child: Text(
-                          item.price.toString(),
-                          textAlign: TextAlign.end,
+                elevation: 4, // Bayangan pada Card
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10), // Sudut melengkung
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 10), // Ruang di atas gambar
+                    Hero(
+                      tag: item.imageUrl, // Unik untuk setiap item
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10),
+                        ),
+                        child: Image.network(
+                          item.imageUrl,
+                          height: 150, // Tinggi gambar
+                          width: double.infinity, // Lebar gambar menyesuaikan
+                          fit: BoxFit.cover, // Gambar sesuai ukuran
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        item.name,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Text('Rp ${item.price}'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Text('Stock: ${item.stock}'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Row(
+                        children: [
+                          Icon(Icons.star, color: Colors.yellow, size: 16),
+                          Text('${item.rating}'),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
             );
           },
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          padding: EdgeInsets.all(5),
+          color: Colors.grey[200],
+          child: Text(
+            'Icha Dewi Putriana | NIM: 2241720069',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 16,
+              fontStyle: FontStyle.italic,
+              color: Colors.grey[600],
+            ),
+          ),
         ),
       ),
     );
--- Pertemuan 6/belanja/lib/pages/item_page.dart
+++ Pertemuan 6/belanja/lib/pages/item_page.dart
@@ -13,12 +13,118 @@ class ItemPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Shopping List'),
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.lightGreen,
       ),
-      body: Center(
-        child: Text(
-          '${item.name} with ${item.price}',
-          style: TextStyle(fontSize: 20),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Menampilkan gambar produk
+              Center(
+                child: Hero(
+                  tag: item.imageUrl, // Sama dengan tag di HomePage
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          blurRadius: 8,
+                          spreadRadius: 2,
+                          offset: Offset(0, 4),
+                        ),
+                      ],
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Image.network(
+                        item.imageUrl,
+                        height: 500, // Atur tinggi gambar sesuai kebutuhan
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 24), // Memberi jarak antara gambar dan konten
+              // Nama produk
+              Text(
+                item.name,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 16),
+              // Harga dan Rating
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Rp.${item.price}',
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.green,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.star, color: Colors.yellow, size: 24),
+                      SizedBox(width: 4),
+                      Text(
+                        '${item.rating}',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              // Stok produk
+              Text(
+                'Stok tersedia: ${item.stock}',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.grey[700],
+                ),
+              ),
+              SizedBox(height: 24),
+              // Tombol "Tambah ke Keranjang"
+              Center(
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    // Aksi ketika tombol ditekan
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('${item.name} ditambahkan ke keranjang!'),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                    backgroundColor: Colors.lightGreen, // Warna tombol
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          20), // Membuat sudut tombol melengkung
+                    ),
+                  ),
+                  icon: Icon(Icons.shopping_cart, size: 24),
+                  label: Text(
+                    'Tambah ke Keranjang',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
